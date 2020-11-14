41467fadb78b749f21f9dec7b5a26afdf64757b8
LaBrew T2 <labrewt2@hawkmail.newpaltz.edu>
2020-11-14 14:34:44 -0500

    .

--- express-sequelize-box-main/routes/api-routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const commentsRoute = require("./comments");
-
-// go to /api/comments
-router.use("/comments", commentsRoute);
-
-module.exports = router;
