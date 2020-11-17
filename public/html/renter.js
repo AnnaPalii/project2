01621f2a6db9520daa2174073a5366d8813125cc
Nuchana <nuchana.johnson@gmail.com>
2020-11-16 20:51:53 -0500

    update db again

--- public/html/renter.js
+++ public/html/renter.js
@@ -1,65 +1,38 @@
 $(document).ready(function () {
-  // Getting references to the name input and renter profile, as well as the table body
-  var renterInput = $("#renter-name");
-  var renterList = $("tbody");
-  var renterContainer = $(".renter-container");
-  // Adding event listeners to the form to create a new object, and the button to delete
-  // an renter
-  $(document).on("submit", "#renter-form", createRenterRow);
 
-  // Getting the initial list of renters
-  getRenters();
-
-
-  // Function for creating a new list row for renters
-  function createRenterRow(event) {
+  // Getting jQuery references to the renter body, title, form, and category select
+  var renterName = $("#renterName");
+  var dimensionInput = $("#renterSpace");
+  var myFile = $("#myImg");
+  var renterZipCode = $("#renterZip");
+  // Giving the renterCategorySelect a default value
+  // renterCategorySelect.val("Personal");
+  // Adding an event listener for when the form is submitted
+  $(renterForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
-
-    var newTr = $("<tr>");
-    newTr.data("renter", renterData);
-    newTr.append("<td>" + renterData.name + renterData.dimension + renterData.zipcode + "</td>");
-    if (renterData.name) {
-      newTr.append("<td> " + renterData.name.length + "</td>");
-    } else {
-      newTr.append("<td>0</td>");
+    // Wont submit the renter if we are missing a body or a title
+    if (!renterName.val().trim() || !dimensionInput.val().trim() || myFile.val().trim() || !renterZipCode.val().trim()) {
+      return;
     }
-    newTr.append("<td><a href='/results?renter_id=" + renterData.id + "'>Go to Results</a></td>");
-    
-    return newTr;
-  }
+    // Constructing a newRenter object to hand to the database
+    var newRenter = {
+      renterfullname: renterName.val().trim(),
+      dimensions: dimensionInput.val().trim(),
+      inputzip: renterZipCode.val(),
+      file: myFile.val()
+    };
 
-  // Function for retrieving renters and getting them ready to be rendered to the page
-  function getRenters() {
-    $.get("/api/renters", function (data) {
-      var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createRenterRow(data[i]));
-      }
-      renderRenterList(rowsToAdd);
-      renterInput.val("");
+    console.log(newRenter);
+
+    submitForm(newRenter);
+
+  });
+
+  // Submits a new renter and brings user to blog page upon completion
+  function submitForm(Renter) {
+    $.renter("/api/renterinput/", Renter, function () {
+      window.location.href = "/results";
     });
+
   }
-
-  // A function for rendering the list of renters to the page
-  function renderRenterList(rows) {
-    renterList.children().not(":last").remove();
-    renterContainer.children(".alert").remove();
-    if (rows.length) {
-      console.log(rows);
-      renterList.prepend(rows);
-    }
-    else {
-      renderEmpty();
-    }
-  }
-
-  // Function for handling what to render when there are no renters
-  function renderEmpty() {
-    var alertDiv = $("<div>");
-    alertDiv.addClass("alert alert-danger");
-    alertDiv.text("You must create an renter before you can get the results.");
-    renterContainer.append(alertDiv);
-  }
-
-
 });
