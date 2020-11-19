2ab8df5bc4edd2e9fe9b015b3e15b07aaa54992e
Nuchana <nuchana.johnson@gmail.com>
2020-11-19 18:33:21 -0500

    fix db

--- public/html/renter.js
+++ public/html/renter.js
@@ -2,24 +2,23 @@ $(document).ready(function () {
 
   // Getting jQuery references to the renter body, title, form, and category select
   var renterName = $("#renterName");
-  var dimensionInput = $("#renterSpace");
-  // var myFile = $("#myImg");
-  var renterZipCode = $("#renterZip");
+  var inputEmail = $("#inputEmail");
+ 
   // Giving the renterCategorySelect a default value
   // renterCategorySelect.val("Personal");
   // Adding an event listener for when the form is submitted
   $(renterForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
-    // Wont submit the renter if we are missing a body or a title
-    if (!renterName.val().trim() || !dimensionInput.val().trim() || !renterZipCode.val().trim()) {
+    // Wont submit the renter if we are missing renter name or e-mail
+    if (!renterName.val().trim() || !inputEmail.val().trim()) {
       return;
     }
     // Constructing a newRenter object to hand to the database
     var newRenter = {
       renterfullname: renterName.val().trim(),
-      dimensions: dimensionInput.val().trim(),
-      inputzip: renterZipCode.val(),
-      // file: myFile.val()
+      inputEmail: inputEmail.val().trim(),
+      
+      
     };
 
     console.log(newRenter);
@@ -30,8 +29,8 @@ $(document).ready(function () {
 
   // Submits a new renter and brings user to blog page upon completion
   function submitForm(Renter) {
-    $.renter("/api/renterinput/", Renter, function () {
-      window.location.href = "/results";
+    $.renter("/api/renterinput", Renter, function () {
+      window.location.href = "/object";
     });
 
   }
